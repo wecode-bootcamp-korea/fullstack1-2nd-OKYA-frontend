f8e7c6defbe11294dc3d31706b70f62dacc63b5d
samankey <71953671+samankey@users.noreply.github.com>
2021-07-28 16:13:35 +0900

    Add : Container 에 Nav, Footer 추가, Routes.jsx에서 제외

---
+++ src/Routes.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Main from './pages/Main/Main.jsx';
+import List from './pages/List/List.jsx';
+import Login from './pages/Login/Login.jsx';
+import Signup from './pages/Signup/Signup.jsx';
+import Cart from './pages/Cart/Cart.jsx';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/list" component={List} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/cart" component={Cart} />
+          <Redirect path="*" to="/" />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
